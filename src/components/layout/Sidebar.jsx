e9fe6d09fbcbacdaba8fda7df747cf2e04d51b20
Satwik1428 <santoshsatwik733@gmail.com>
2026-04-18 16:28:13 +0530

    Ui improvement and sidebar changes

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,18 +1,37 @@
 import "../../styles/sidebar.css";
-import logo from "../../assets/logo.jpeg"
+import logo from "../../assets/logo.jpeg";
+import {Home, Wallet, Target, Timer, LogOut} from "lucide-react";
 function Sidebar()
 {
     return(
-        <aside className="Sidebar">
+        <aside className="sidebar">
             <div className="logo">
                 <img src={logo} alt="whilist-logo"></img>
             </div>
             <nav className="sections">
                 <ul>
-                    <li>Home</li>
-                    <li>Earnings</li>
-                    <li>Wishlist</li>
-                    <li>Countdowns</li>
+                    <li>
+                        <a href="#" onClick={(e) => e.preventDefault()}>
+                            <Home size={18} />
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" onClick = {(e) => e.preventDefault()}>
+                            <Wallet size={18} />
+                            <span>Earnings</span>
+                        </a>
+                    </li>
+                    <li><a href="#" onClick={(e) => e.preventDefault()}>
+                        <Target size={18} />
+                        <span>Wishlist</span>
+                        </a>
+                    </li>
+                    <li><a href="#" onClick={(e) => e.preventDefault()}>
+                        <Timer size={18} />
+                        <span>Countdown</span>
+                        </a>
+                    </li>
                 </ul>
             </nav>
             <div className="logout">
