1471596ec28e74894abdae6c9e753b89ca538d86
Satwik1428 <santoshsatwik733@gmail.com>
2026-05-16 18:06:24 +0530

    code review

--- src/components/wishlist/GoalGrid.jsx
+++ src/components/wishlist/GoalGrid.jsx
@@ -1,7 +1,47 @@
 import GoalCard from "./GoalCard"
 function GoalGrid(){
+    const data = [
+        {
+            name: "Spurs away jersey",
+            target: "9000",
+            saved: "1000",
+            targetDate: "2024-12-31",
+            priority: "High",
+        },
+        {
+            name: "Tshirt",
+            target: "1500",
+            saved: "500",
+            targetDate: "2024-12-31",
+            priority: "Medium",
+        },
+        {
+            name: "Beige pants",
+            target: "2000",
+            saved: "1500",
+            targetDate: "2024-12-31",
+            priority: "High",
+        },
+        {
+            name: "Shoes",
+            target: "4000",
+            saved: "500",
+            targetDate: "2024-12-31",
+            priority: "Low",
+        }
+    ]
     return(
-        <GoalCard></GoalCard>
+        data.map((item) => {
+            return(
+                <GoalCard 
+                key = {item.id}
+                name={item.name}
+                target={item.target}
+                saved={item.saved}
+                targetDate={item.targetDate}
+                priority = {item.priority} />
+            )
+        })
     )
 }
 export default GoalGrid
